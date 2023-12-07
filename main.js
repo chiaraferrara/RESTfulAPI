50388368285577c41e4186a5fcf6acbc6c702f8e
c.ferrara <chiara.ferrara@stevejobs.academy>
2023-12-07 11:54:48 +0100

    feat(routes.js): get, post, delete contact

--- main.js
+++ main.js
@@ -1,50 +1,59 @@
-const http = require('http');
-var express = require('express');
-var app =express();
-var routes = require('./routes.js');
-var mysql = require('mysql');
+const express = require('express');
+const app = express();
+const routes = require('./routes.js');
+const mysql = require('mysql2');
+app.use(express.json()); //middleware che viene eseguito ogni volta che viene fatta una request e una response.
 
-var connection = mysql.createConnection({
-    host: "localhost",
+const connection = mysql.createConnection({
+    host: "localhost",    
     user: "root",
-    password: "",
-    database: "NodeDB"
+    password: "",    
+    database: "nodedb",
+    port:"3306",
+});
 
-})
 
-//L'oggetto connessione creato nell'esempio precedente dispone di un metodo per eseguire
-// query sul database
-connection.connect(function(err){
-    if(err) throw err;
-    console.log('Connection created');
-    connection.query(sql, function(err, result){
-        if(err) throw err;
-        console.log("Result: " +  result)
-    })
-})
+connection.connect(function(err) {
+    if (err) {
+        console.error('Errore: ', err);
+        return;
+    }
 
-connection.connect(function(err){
-    if(err) throw err;
-    connection.query("CREATE DATABASE NodeDB", function(err,result){
-        if(err) throw err;
-        console.log("DATABASE CREATO");
+    console.log('Connesso al database');
+
+
+    connection.query("CREATE DATABASE IF NOT EXISTS NodeDB", function(err, result) {
+        if (err) {
+            console.error('Errore: ', err);
+            return;
+        }
+        console.log("Database creato o già esistente");
+        
+ 
+        connection.query("USE NodeDB", function(err, result) {
+            if (err) {
+                console.error('Errore: ', err);
+                return;
+            }
+            
+ 
+            const sql = "CREATE TABLE IF NOT EXISTS contatti (id INT(100),nome VARCHAR(255), telefono VARCHAR(255), email VARCHAR(255))";
+            connection.query(sql, function(err, result) {
+                if (err) {
+                    console.error('Errore: ', err);
+                    return;
+                }
+                console.log("Tabella 'contatti' creata o già esistente");
+            });
+        });
     });
 });
-connection.connect(function(err){
-    if(err) throw err;
-    console.log("Connected");
-    var sql = "CREATE TABLE contatti (nome VARCHAR(255), telefono VARCHAR(255), email VARCHAR(255))";
-    connection.query(sql, function(err, result){
-        if(err) throw err;
-        console.log("Tabella creata");
-    })
-})
 
-connection.connect(function(err){
+// Use routes for '/contatti'
+app.use('/contatti', routes);
 
-})
-
-app.use('/contatti', routes)
-const router = express.Router();
-// const server = http.createServer(router);
-app.listen(3000)
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Porta: ${PORT}`);
+});
--- routes.js
+++ routes.js
@@ -1,26 +1,79 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
+
+
+const connection = mysql.createConnection({
+    host: "localhost",    
+    user: "root",
+    password: "",    
+    database: "nodedb",
+    port:"3306",
+});
+
 
 
 router.get('/', function(request, response){
-    response.send("GET CONTATTI")
+    const query = "SELECT * FROM `contatti`";
+    connection.query(query, function(err, result){
+        if(err){
+            console.error('Errore: ', err);
+            return;
+        }
+       response.send(result);
+    });
+    
 })
 
 
 router.get('/:id', function(request,response){
-    response.send("L'id del contatto è: "  +   request.params.id );
+    const id = request.params.id
+    const query = "SELECT * FROM `contatti` WHERE `id` = " + id;
+    connection.query(query, [id], function(err, result){
+        if(err){
+            console.error('Errore: ', err)
+            return;
+        }
+        response.send(result);
+    })
 })
 
 router.post('/', function(request,response){
-    response.send("POST CONTATTI");
+    // const id = request.body.id;
+    const nome = request.body.nome;
+    const telefono = request.body.telefono;
+    const email = request.body.email;
+    response.send("POST CONTATTI: " + nome + "<br/> " + telefono + "<br/>" + email);
+    // response.send("Contatto inserito.")
+
+    const query = "INSERT INTO `contatti`(`nome`, `telefono`, `email`) VALUES (?,?,?)";
+            connection.query(query,[nome, telefono, email], function(err, result) {
+                if (err) {
+                    console.error('Errore: ', err);
+                    return;
+                }
+                console.log("Contatto Inserito" + nome + " " + telefono + " " + email);
+                
+            });
 })
 
 router.put('/:id', function(request,response){
-    response.send("PUT CONTATTI: " + request.params.id);
+   response.send('PUT DI ID');
 })
 
 router.delete('/:id',function(request,response){
-    response.send("DELETE CONTATTI: " + request.params.id);
+    const id= request.params.id;
+    const query = "DELETE FROM `contatti` WHERE id=" + id;
+    connection.query(query,[id], function(error, results){
+        if(error){
+            console.error(err);
+            return;
+        }
+        console.log("Contatto eliminato: " + results.affectedRows)
+        response.send("Contatto eliminato con successo")
+    })
 })
 
 router.all('*', (request, response) => {
